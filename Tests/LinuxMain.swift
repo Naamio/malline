feba5cc37785ea07506953b5ad5a7c8c1afdc017
Phil J. Laszkowicz <phil@fillip.pro>
2017-12-24 15:13:52 +0200

    Includes lexer test suite for #1.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -3,6 +3,7 @@ import XCTest
 @testable import MallineTests
 
 XCTMain([
+    testCase(LexerTests.allTests),
     testCase(LoaderTests.allTests),
     testCase(ParserTests.allTests),
     testCase(StencilTests.allTests),
